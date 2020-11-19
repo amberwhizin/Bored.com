fe5a49f7ff1cdf12af554576944c60fbc411bca8
TaviaC91 <TaviaC91@gmail.com>
2020-11-19 12:05:18 -0500

    TC

--- whatabore_client/src/App.js
+++ whatabore_client/src/App.js
@@ -2,19 +2,7 @@ import React, { Component } from "react";
 import Index from './components/Index'
 
 export default class App extends Component {
-<<<<<<< HEAD
-    render() {
-        return (
-            <div>
 
-                Hello!
-
-                Hey!!
-
-            </div>
-        )
-    }
-=======
   constructor(props) {
     super(props);
     this.state = {
@@ -25,19 +13,14 @@ export default class App extends Component {
   }
   
   render() {
-dani
+
     return <div>
         <Index />
        
         </div>;
-    return (
-      <div>
-        <input type="text" />
-        <input type="submit" value="here" />
-        Hey!!
-      </div>
-    );
-dev
+    
+      
+    
+
   }
->>>>>>> 74c62440f6f9832fde9d9786493a987ae31b0c3d
 }
