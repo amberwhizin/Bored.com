ea948e4d0ce2fd224f82febe1d6afe76a1ab46cd
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-11-28 22:58:49 -0500

    Fix routiung issue on heroku (finally...)
    https://stackoverflow.com/questions/61515999/304-status-on-page-refresh

--- whatabore_client/src/components/todolist-component/TodoList.js
+++ whatabore_client/src/components/todolist-component/TodoList.js
@@ -28,7 +28,7 @@ export default class TodoList extends Component {
       return i !== index;
     });
     const currentItem = this.state.items[index];
-    fetch("/todo-lists/" + currentItem._id, {
+    fetch("/api/todo-lists/" + currentItem._id, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -64,7 +64,7 @@ export default class TodoList extends Component {
     const currentItem = items[index];
     //id is being passes in with "params" from back end
     // console.log(this.state, { index, items, currentItem });
-    fetch("/todo-lists/" + currentItem._id, {
+    fetch("/api/todo-lists/" + currentItem._id, {
       method: "PUT",
       body: JSON.stringify({
         name: currentItem.name,
@@ -89,7 +89,7 @@ export default class TodoList extends Component {
   handleSubmitOnItem = (e) => {
     e.preventDefault();
     if (!this.state.name) return;
-    fetch("/todo-lists/", {
+    fetch("/api/todo-lists/", {
       method: "POST",
       body: JSON.stringify({
         name: this.state.name,
@@ -108,7 +108,7 @@ export default class TodoList extends Component {
   };
 
   getItem = () => {
-    fetch("/todo-lists")
+    fetch("/api/todo-lists")
       .then((data) => {
         return data.json();
       })
